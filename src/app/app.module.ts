db4ad73e8361ffd3e1815a80750e5fb082f12a51
Maina Wycliffe <wmmaina7@gmail.com>
2020-04-29 02:47:49 +0300

    Initial Demo Skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { CdkStepperModule } from '@angular/cdk/stepper';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MyStepperComponent } from './stepper/my-stepper/my-stepper.component';
@@ -9,7 +10,12 @@ import { MyStepperHeaderComponent } from './stepper/my-stepper-header/my-stepper
 
 @NgModule({
   declarations: [AppComponent, MyStepperComponent, MyStepperHeaderComponent],
-  imports: [BrowserModule, CdkStepperModule, FontAwesomeModule],
+  imports: [
+    BrowserModule,
+    CdkStepperModule,
+    FontAwesomeModule,
+    ReactiveFormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
