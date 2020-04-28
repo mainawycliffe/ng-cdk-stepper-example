35df1b152a85f473c6f739d09b6420d9b4bc7fbc
Maina Wycliffe <wmmaina7@gmail.com>
2020-04-28 23:16:59 +0300

    Installl Font Awesome Module For Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { CdkStepperModule } from '@angular/cdk/stepper';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppComponent } from './app.component';
 import { MyStepperComponent } from './stepper/my-stepper/my-stepper.component';
 
 @NgModule({
   declarations: [AppComponent, MyStepperComponent],
-  imports: [BrowserModule, CdkStepperModule],
+  imports: [BrowserModule, CdkStepperModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent],
 })
