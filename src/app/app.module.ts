735119330b2f436dd7725bbc95b2d7eebe01b163
Maina Wycliffe <wmmaina7@gmail.com>
2020-05-24 22:14:19 +0300

    Remove no form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,16 +9,11 @@ import { AppComponent } from './app.component';
 import { MyStepperComponent } from './stepper/my-stepper/my-stepper.component';
 import { SingleFormComponent } from './examples/single-form/single-form.component';
 import { MultipleFormsComponent } from './examples/multiple-forms/multiple-forms.component';
-import { NoFormComponent } from './examples/no-form/no-form.component';
 
 const routes: Route[] = [
   {
     path: '',
     component: MultipleFormsComponent,
-  },
-  {
-    path: 'no-form',
-    component: NoFormComponent,
   },
   {
     path: 'single-form',
@@ -36,7 +31,6 @@ const routes: Route[] = [
     MyStepperComponent,
     SingleFormComponent,
     MultipleFormsComponent,
-    NoFormComponent,
   ],
   imports: [
     BrowserModule,
